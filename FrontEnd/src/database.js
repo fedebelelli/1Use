f7c88f28954fef952e7e1d11e9975c62773d40c3
fedebelelli <fede.belelli@gmail.com>
2019-07-03 17:50:41 -0300

    Update database.js

--- FrontEnd/src/database.js
+++ FrontEnd/src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { mongodb } = require('./keys') 
 
-mongoose.connect(mongodb.URI, {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://fede:1use@cluster0-pdt0d.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true})
 .then(db => console.log('La base de datos esta conectada'))
 .catch(err => console.error(err));
