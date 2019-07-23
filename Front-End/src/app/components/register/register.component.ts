4a02ec35f8c21eebca3e8cde62d5270ef87f29a8
Juan VH <juanivanheer@gmail.com>
2019-07-23 13:30:20 -0300

    Arreglado botón de registrar usuario

--- Front-End/src/app/components/register/register.component.ts
+++ Front-End/src/app/components/register/register.component.ts
@@ -19,40 +19,29 @@ export class RegisterComponent implements OnInit {
   registerUserData = {}
 
   emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  passwordPattern:  " ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$";
+  passwordPattern: " ^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}$";
 
-constructor( private _auth: AuthService, private FormBuilder: FormBuilder, private _router: Router ){ 
-  
-}
-
-
- 
+  constructor(private _auth: AuthService, private FormBuilder: FormBuilder, private _router: Router) { }
 
   ngOnInit() {
-     this.formGroup = this.FormBuilder.group({
-       password: ['', [Validators.required]],
-       password2: ['', [Validators.required]],
-       name: ['', [Validators.required]],
+    this.formGroup = this.FormBuilder.group({
+      password: ['', [Validators.required]],
+      password2: ['', [Validators.required]],
+      name: ['', [Validators.required]],
       email: ['', [Validators.required]],
-     }, { validator: passwordMatchValidator });
+    }, { validator: passwordMatchValidator });
   }
 
-  registerUser(){
-
-    this ._auth.registerUser(this.registerUserData).subscribe(
-
-        res => {
-          
-          console.log(res)
-          localStorage.setItem('token', res.token)
-          this._router.navigate(['/home'])
-        },
-        err => console.log(err)
-
-      )
-
+  registerUser() {
+    this._auth.registerUser(this.registerUserData).subscribe(
+      res => {
+        console.log(res)
+        localStorage.setItem('token', res.token)
+        this._router.navigate(['/home'])
+      },
+      err => console.log(err)
+    )
     //console.log(this.registerUserData)
-
   }
 
   onSaveForm() {
@@ -65,25 +54,24 @@ constructor( private _auth: AuthService, private FormBuilder: FormBuilder, priva
   }
 
 
-   /* Shorthands for form controls (used from within template) */
-   get password() { return this.formGroup.get('password'); }
-   get password2() { return this.formGroup.get('password2'); }
-   get name() { return this.formGroup.get('name'); }
-   get email() { return this.formGroup.get('email'); }
-   /* Called on each input in either password field */
-   onPasswordInput() {
-     if (this.formGroup.hasError('passwordMismatch'))
-       this.password2.setErrors([{ 'passwordMismatch': true }]);
-     else
-       this.password2.setErrors(null);
-   }
+  /* Shorthands for form controls (used from within template) */
+  get password() { return this.formGroup.get('password'); }
+  get password2() { return this.formGroup.get('password2'); }
+  get name() { return this.formGroup.get('name'); }
+  get email() { return this.formGroup.get('email'); }
+  /* Called on each input in either password field */
+  onPasswordInput() {
+    if (this.formGroup.hasError('passwordMismatch'))
+      this.password2.setErrors([{ 'passwordMismatch': true }]);
+    else
+      this.password2.setErrors(null);
+  }
+}
 
- }
-
- export const passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
-   if (formGroup.get('password').value === formGroup.get('password2').value)
-     return null;
-   else
-     return { passwordMismatch: true };
- };
+export const passwordMatchValidator: ValidatorFn = (formGroup: FormGroup): ValidationErrors | null => {
+  if (formGroup.get('password').value === formGroup.get('password2').value)
+    return null;
+  else
+    return { passwordMismatch: true };
+};
 
