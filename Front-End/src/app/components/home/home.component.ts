d16c63a4ec954543afd353d5d173d39c4ebf042b
romorales <roma.morales12@gmail.com>
2019-08-04 21:55:43 -0300

    Home - Banner con Publicar - Categorias

--- Front-End/src/app/components/home/home.component.ts
+++ Front-End/src/app/components/home/home.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { SingletonService } from '../singleton.service'
+import { SingletonService } from '../singleton.service';
+import { CategoriasComponent } from '../../components/categorias/categorias.component'; 
+
+@NgModule({
+  imports: [ CategoriasComponent ]
+})
 
 @Component({
   selector: 'app-home',
