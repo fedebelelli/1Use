3bd5cd7a897cd59a6e60308d5f9b04847eee721a
Juan VH <juanivanheer@gmail.com>
2019-08-15 21:15:21 -0300

    Agregado Swipper en /home

--- Front-End/src/app/components/home/home.component.ts
+++ Front-End/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SingletonService } from '../singleton.service';
-import { CategoriasComponent } from '../../components/categorias/categorias.component'; 
+import { CategoriasComponent } from '../../components/categorias/categorias.component';
+import { SwiperComponent, SwiperConfigInterface} from 'ngx-swiper-wrapper';
 
 @NgModule({
-  imports: [ CategoriasComponent ]
+  imports: [CategoriasComponent]
 })
 
 @Component({
@@ -15,17 +16,42 @@ import { CategoriasComponent } from '../../components/categorias/categorias.comp
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild(SwiperComponent, { static: false }) componentRef?: SwiperComponent;
+
   inicioSesion: boolean;
   urlActual: string;
   urlRecortada: string;
   usuarioActivo;
-  
 
-   constructor(private singleton: SingletonService) { }
+  public slides = [
+    'First slide',
+    'Second slide',
+    'Third slide',
+    'Fourth slide',
+    'Fifth slide',
+    'Sixth slide'
+  ];
+
+  constructor(private singleton: SingletonService) { }
 
 
-   ngOnInit() {
+  ngOnInit() {
 
   }
+
+  //SWIPER
+
+  public config: SwiperConfigInterface = {
+    a11y: true,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    keyboard: true,
+    mousewheel: false,
+    scrollbar: false,
+    navigation: true,
+    pagination: false,
+    autoplay: { delay: 5000},
+  };
+
 
 }
