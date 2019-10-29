e52d490ee96764888ae7f5d5ab64cc29e6934fa5
Juan VH <juanivanheer@gmail.com>
2019-10-29 14:35:58 -0300

    Estadisticas: visita en publicación por usuario terminada

--- Front-End/src/app/components/mi-cuenta/mi-cuenta.component.ts
+++ Front-End/src/app/components/mi-cuenta/mi-cuenta.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SingletonService } from '../singleton.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { MatSelect } from '@angular/material';
 
 @Component({
   selector: 'app-mi-cuenta',
@@ -9,22 +10,56 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class MiCuentaComponent implements OnInit {
 
-  publicaciones: boolean = false;
   alquileres: boolean = false;
   perfil: boolean = false;
+  publicaciones = [];
+  dataSource;
+  hayPublicaciones = false;
 
   constructor(private singleton: SingletonService, private _auth: AuthService) { }
 
-  ngOnInit(){
-    this._auth.get_visitas_id('5d68670f3c1ee86964c5e05c').subscribe(
+  ngOnInit() {
+    this._auth.get_publicacion(localStorage.getItem('email')).subscribe(
       res => {
-        let array = res.doc;
-        console.log(array)
+        this.publicaciones = res.publicaciones;
       }
-    )}
+    )
+  }
 
   cerrarSesion() {
     this.singleton.cerrarSesion();
+  }
+
+  cargarDatos(id, titulo) {
+    this._auth.get_visitas_id(id).subscribe(
+      res => {
+        if (res.doc.length > 1) {
+          this.hayPublicaciones = true;
+          let array = res.doc;
+          let arrayObjetos = []
+          for (let i = 0; i < array.length; i++) {
+            let objeto = { label: array[i].fecha_visita, value: array[i].cantidadVisitas }
+            arrayObjetos.push(objeto)
+          }
+
+          this.dataSource = {
+            // Chart Configuration
+            "chart": {
+              "caption": "Cantidad de visitas en '" + titulo + "'",
+              "subCaption": "En un período de tiempo",
+              "xAxisName": "Tiempo",
+              "yAxisName": "Cantidad de visitas",
+              "numberSuffix": " ",
+              "theme": "fusion"
+            },
+            "data": arrayObjetos
+          }
+        } else {
+          this.hayPublicaciones = false;
+          this.dataSource = undefined;
+        }
+      }
+    )
   }
 
   cambioTab(evento) {
