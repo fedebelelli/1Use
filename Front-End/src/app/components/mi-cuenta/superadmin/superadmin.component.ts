ef121c979e5b090a41200402ca3efa7a7a76a7b6
Juan VH <juanivanheer@gmail.com>
2020-04-04 17:29:24 -0300

    Botones de inicio superadmin

--- Front-End/src/app/components/mi-cuenta/superadmin/superadmin.component.ts
+++ Front-End/src/app/components/mi-cuenta/superadmin/superadmin.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SingletonService } from '../../singleton.service';
 
+export interface Botones {
+  icono: string,
+  texto: string,
+  link: string,
+}
+
 @Component({
   selector: 'app-superadmin',
   templateUrl: './superadmin.component.html',
@@ -16,5 +22,14 @@ export class SuperadminComponent implements OnInit {
   cerrarSesion() {
     this.singleton.cerrarSesion();
   }
+
+  botones: Botones[] = [
+    { icono: "account_box", texto: "Usuarios", link: "/mi-cuenta/superadmin/usuarios" },
+    { icono: "storefront", texto: "Publicaciones", link: "/mi-cuenta/superadmin/publicaciones" },
+    { icono: "dashboard", texto: "Alquileres", link: "/mi-cuenta/superadmin/alquileres" },
+    { icono: "menu_book", texto: "Reclamos", link: "/mi-cuenta/superadmin/reclamos" },
+    { icono: "assessment", texto: "Estadísticas", link: "/mi-cuenta/superadmin/estadisticas" },
+    { icono: "exit_to_app", texto: "Salir", link: "" },
+  ]
 
 }
