106f4c884afc4a277093ef5bb82dc0030ca51087
Juan VH <juanivanheer@gmail.com>
2020-04-10 16:18:49 -0300

    Cambios random en estadisticas

--- Front-End/src/app/components/mi-cuenta/superadmin/estadisticas/estadisticas.component.ts
+++ Front-End/src/app/components/mi-cuenta/superadmin/estadisticas/estadisticas.component.ts
@@ -10,6 +10,14 @@ export class EstadisticasComponent implements OnInit {
 
   constructor(private singleton: SingletonService) { }
 
+  estadisticas: string[] = [
+    'Cantidad de visitantes al sitio en un periodo de tiempo', 'Cantidad de alquileres según categoría', 'Cantidad de publicaciones según categoría',
+    'Cantidad de publicaciones dadas de baja por infringir los términos y condiciones', 'Usuarios que más alquilan objetos', 'Usuarios que más publican en un periodo de tiempo',
+    'Usuarios mejores puntuados del sitio por tipo (publicador/locatario)', 'Objetos más alquilados por propietario', 'Cantidad de ingresos monetarios al sitio por mes'
+  ];
+
+  
+
   ngOnInit() {
   }
 
